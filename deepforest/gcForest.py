a2e75efd93c10fd46ce409c2d2e882feecb8fa0b
AleAle <867628671@qq.com>
2025-01-21 21:51:50 +0800

    complete the "train_and_predict" method in gcForest.py and test it

--- deepforest/gcForest.py
+++ deepforest/gcForest.py
@@ -20,12 +20,14 @@ class gcForest:
         Train the gcForest.All layers are stored in the member variable layer_list.
         :param train_data:
         :param train_label:
-        :return:    val_acc: a list of every layer's validation accuracy
+        :return:    val_p: a list of every layer's predicted probability.
+                    val_acc: a list of every layer's validation accuracy.
                     best_layer_index:
         """
         train_data_raw = train_data.copy()
         layer_index = 0
         val_acc = []
+        val_p = []
         best_acc = 0
         bad = 0
         best_layer_index = 0
@@ -33,10 +35,11 @@ class gcForest:
         while layer_index <= self.max_layer:
             layer = Layer(self.num_forests, self.num_estimator, self.num_classes, self.n_fold, layer_index,
                           self.max_depth, 1)
-            val_prob, feature_new = layer.train(train_data,train_label)
+            val_prob, feature_new = layer.train(train_data, train_label)
             self.layer_list.append(layer)
             accuracy = compute_accuracy(train_label, val_prob)
             val_acc.append(accuracy)
+            val_p.append(val_prob)
 
             train_data = np.concatenate([train_data_raw, feature_new], axis=1)
             train_data = np.float16(train_data)
@@ -47,9 +50,68 @@ class gcForest:
                 best_layer_index = layer_index
                 bad = 0
             else:
-                bad+=1
-                if bad >self.tolerance:
-                    break
+                bad += 1
             layer_index+=1
-            self.best_layer = best_layer_index
-        return [val_acc,best_layer_index]
+            if bad > self.tolerance:
+                break
+        self.number_of_layers = layer_index
+        self.best_layer = best_layer_index
+        return [val_p, val_acc, best_layer_index]
+
+    def train_and_predict(self, train_data, train_label, test_data, test_label):
+        """
+        Train the gcForest.All layers are stored in the member variable layer_list.
+        And then predict the probability on test data.
+        :param train_data:
+        :param train_label:
+        :param test_data:
+        :param test_label:
+        :return:    val_p: a list of every layer's predicted probability on train data.
+                    val_acc: a list of every layer's validation accuracy.
+                    test_p: a list of every layer's predicted probability on test data.
+                    test_acc: a list of every layer's test accuracy
+                    best_layer_index:
+        """
+        train_data_raw = train_data.copy()
+        test_data_raw = test_data.copy()
+        layer_index = 0
+        val_p = []
+        val_acc = []
+        best_acc = 0
+        test_p = []
+        test_acc = []
+        bad = 0
+        best_layer_index = 0
+
+        while layer_index <= self.max_layer:
+            layer = Layer(self.num_forests, self.num_estimator, self.num_classes, self.n_fold, layer_index,
+                          self.max_depth, 1)
+            val_prob, feature_new, test_prob, test_feature_new = layer.train_and_predict(train_data, train_label,
+                                                                                         test_data)
+            self.layer_list.append(layer)
+            accuracy = compute_accuracy(train_label, val_prob)
+            accuracy_test = compute_accuracy(test_label, test_prob)
+            val_acc.append(accuracy)
+            val_p.append(val_prob)
+            test_acc.append(accuracy_test)
+            test_p.append(test_prob)
+
+            train_data = np.concatenate([train_data_raw, feature_new], axis=1)
+            train_data = np.float16(train_data)
+            train_data = np.float64(train_data)
+            test_data = np.concatenate([test_data_raw, test_feature_new], axis=1)
+            test_data = np.float16(test_data)
+            test_data = np.float64(test_data)
+
+            if accuracy > best_acc:
+                best_acc = accuracy
+                best_layer_index = layer_index
+                bad = 0
+            else:
+                bad += 1
+            layer_index += 1
+            if bad > self.tolerance:
+                break
+        self.number_of_layers = layer_index
+        self.best_layer = best_layer_index
+        return [val_p, val_acc, test_p, test_acc, best_layer_index]
--- test/testgcForest.py
+++ test/testgcForest.py
@@ -5,21 +5,34 @@ from sklearn.model_selection import train_test_split
 from deepforest.Layer import Layer
 from deepforest.gcForest import gcForest
 
-np.random.seed(42)
+#np.random.seed(42)
 
 # 使用 sklearn 的 make_classification 生成多分类数据集
 X, y = make_classification(
     n_samples=500, n_features=20, n_informative=15,
-    n_classes=3, random_state=42
+    n_classes=3
 )
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
 def test_train():
     model = gcForest(num_estimator=100, num_forests=4, num_classes=3)
-    val_acc, best_layer_index = model.train(X_train, y_train)
+    val_p, val_acc, best_layer_index = model.train(X_train, y_train)
     print(val_acc)
     print(best_layer_index)
 
-test_train()
+def test_train_and_predict():
+    model = gcForest(num_estimator=100, num_forests=4, num_classes=3)
+    val_p, val_acc, test_p, test_acc, best_layer_index = model.train_and_predict(X_train, y_train, X_test, y_test)
+    print(val_p[0].shape)
+    print(len(val_p))
+    print(model.number_of_layers)
+    print(val_acc)
+    print(test_p[0].shape)
+    print(test_acc)
+    print(best_layer_index)
+
+
+#test_train()
+test_train_and_predict()
